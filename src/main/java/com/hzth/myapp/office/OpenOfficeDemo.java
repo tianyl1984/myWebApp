98e485eb70cd2b49b7cefc42dadb4a19ea941fb2
tianyl1984 <tianyl1984@qq.com>
2015-03-07 21:28:50 +0800

    完善例子

--- src/main/java/com/hzth/myapp/office/OpenOfficeDemo.java
+++ src/main/java/com/hzth/myapp/office/OpenOfficeDemo.java
@@ -1,36 +1,31 @@
 package com.hzth.myapp.office;
 
-import java.io.File;
-
-import org.artofsolving.jodconverter.OfficeDocumentConverter;
-import org.artofsolving.jodconverter.office.DefaultOfficeManagerConfiguration;
-import org.artofsolving.jodconverter.office.OfficeManager;
 
 public class OpenOfficeDemo {
 
-	private static OfficeDocumentConverter converter;
-
-	private static OfficeManager manager;
-
-	public static void main(String[] args) {
-		init();
-		File file1 = new File("E:/aa.rtf");
-		File file2 = new File("E:/aa.pdf");
-		converter.convert(file1, file2);
-		stop();
-	}
-
-	private static void stop() {
-		if (manager != null) {
-			manager.stop();
-		}
-	}
-
-	private static void init() {
-		DefaultOfficeManagerConfiguration conf = new DefaultOfficeManagerConfiguration();
-		manager = conf.buildOfficeManager();
-		manager.start();
-		converter = new OfficeDocumentConverter(manager);
-	}
+	// private static OfficeDocumentConverter converter;
+	//
+	// private static OfficeManager manager;
+	//
+	// public static void main(String[] args) {
+	// init();
+	// File file1 = new File("E:/aa.rtf");
+	// File file2 = new File("E:/aa.pdf");
+	// converter.convert(file1, file2);
+	// stop();
+	// }
+	//
+	// private static void stop() {
+	// if (manager != null) {
+	// manager.stop();
+	// }
+	// }
+	//
+	// private static void init() {
+	// DefaultOfficeManagerConfiguration conf = new DefaultOfficeManagerConfiguration();
+	// manager = conf.buildOfficeManager();
+	// manager.start();
+	// converter = new OfficeDocumentConverter(manager);
+	// }
 
 }
