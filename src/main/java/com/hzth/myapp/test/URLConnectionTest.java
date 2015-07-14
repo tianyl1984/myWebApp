1ba04653f25384aef91aea3174813877debb93e4
tianyl1984 <tianyl1984@qq.com>
2015-07-14 09:37:22 +0800

    test

--- src/main/java/com/hzth/myapp/test/URLConnectionTest.java
+++ src/main/java/com/hzth/myapp/test/URLConnectionTest.java
@@ -1,75 +1,16 @@
 package com.hzth.myapp.test;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.htmlparser.Node;
-import org.htmlparser.Parser;
-import org.htmlparser.util.NodeIterator;
-import org.htmlparser.util.NodeList;
-
-import com.hzth.myapp.core.util.FileUtil;
-
 public class URLConnectionTest {
 
 	public static void main(String[] args) throws Exception {
-		start();
-	}
-
-	private static void start() {
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				for (int i = 0; i < 100000; i++) {
-					try {
-						System.out.println("--------------查询-----------");
-						String result = getResponse("http://yun.developer.baidu.com/invitation");
-						// System.out.println(result);
-						Parser parser = Parser.createParser(result, "utf-8");
-						NodeIterator ni = parser.elements();
-						ni.nextNode();
-						while (ni.hasMoreNodes()) {
-							Node node = ni.nextNode();
-							printNodeInfo(node);
-						}
-						Thread.sleep(1000);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
-
-	private static void printNodeInfo(Node node) {
-		if (node != null && node.getChildren() != null) {
-			NodeList nodeList = node.getChildren();
-			for (int i = 0; i < nodeList.size(); i++) {
-				Node childNode = nodeList.elementAt(i);
-				if (childNode.toHtml().trim().startsWith("<ul class=\"codelist\">")) {
-					printTargetNode(childNode);
-					break;
-				} else {
-					printNodeInfo(childNode);
-				}
-			}
-		}
-	}
-
-	private static void printTargetNode(Node node) {
-		for (int i = 0; i < node.getChildren().size(); i++) {
-			Node childNode = node.getChildren().elementAt(i);
-			if (childNode.toHtml().trim().length() == 0) {
-				continue;
-			}
-			if (!childNode.toHtml().trim().contains("已被使用")) {
-				System.out.println(childNode.toHtml());
-				FileUtil.append("E:/result.txt", childNode.toHtml().trim() + "\n");
-			}
-		}
+		String url = "http://192.168.90.49:1377/diagnosis/passport/checkToken?token=123";
+		System.out.println(getResponse(url));
 	}
 
 	private static String getResponse(String url) throws Exception {
@@ -85,10 +26,14 @@ public class URLConnectionTest {
 		// 连接
 		conn.connect();
 
+		InputStream is = null;
 		if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
 			System.out.println("异常响应编码：" + conn.getResponseCode());
+			is = conn.getErrorStream();
+		} else {
+			is = conn.getInputStream();
 		}
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		String result = "";
 		String temp = null;
 		while ((temp = reader.readLine()) != null) {
