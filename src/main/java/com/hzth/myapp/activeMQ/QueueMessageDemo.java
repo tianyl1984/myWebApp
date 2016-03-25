c65967b2a4904ece8b61b4e9c90fdbff7655697a
tianyl1984 <tianyl1984@qq.com>
2016-03-25 15:18:47 +0800

    update queue jms demo

--- src/main/java/com/hzth/myapp/activeMQ/QueueMessageDemo.java
+++ src/main/java/com/hzth/myapp/activeMQ/QueueMessageDemo.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -30,74 +29,116 @@ public class QueueMessageDemo {
 	private static final String QUEUE_NAME = "queue_time";
 
 	public static void main(String[] args) {
-		startReceive();
-		// startSend();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				startReceive();
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				startReceive();
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				startReceive();
+			}
+		}).start();
+
+		startSend();
 	}
 
 	private static void startSend() {
 		QueueConnection connection = null;
-		QueueSession session = null;
 		QueueConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKER_URL);
 		try {
 			connection = connectionFactory.createQueueConnection();
 			connection.start();
-			// true 使用session，AUTO_ACKNOWLEDGE 自动应答，自动删除消息
-			session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
-			Queue queue = session.createQueue(QUEUE_NAME);
-			QueueSender sender = session.createSender(queue);
-			sender.setDeliveryMode(DeliveryMode.PERSISTENT);
 
-			System.out.println("开始发送。。。");
-			for (int i = 0; i < 2; i++) {
-				TextMessage tm = session.createTextMessage();
-				tm.setText("time:" + new Date().getTime());
-				Thread.sleep(2000);
-				sender.send(tm);
+			QueueSession session = null;
+			while (true) {
+				try {
+					// true 使用session，AUTO_ACKNOWLEDGE 自动应答，自动删除消息
+					session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
+					Queue queue = session.createQueue(QUEUE_NAME);
+					QueueSender sender = session.createSender(queue);
+					sender.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+					System.out.println("开始发送。。。");
+					for (int i = 0; i < 2; i++) {
+						TextMessage tm = session.createTextMessage();
+						tm.setText("time:" + new Date().getTime());
+						sender.send(tm);
+					}
+					session.commit();
+					System.out.println("发送完成。。。");
+					Thread.sleep(1000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					close(session, null);
+				}
 			}
-			session.commit();
-			System.out.println("发送完成。。。");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			close(session, connection);
+			close(null, connection);
 		}
 	}
 
 	private static void startReceive() {
 		QueueConnection connection = null;
-		QueueSession session = null;
 		QueueConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKER_URL);
 		try {
 			connection = connectionFactory.createQueueConnection();
 			connection.start();
-			// true 使用session，AUTO_ACKNOWLEDGE 自动应答，自动删除消息
-			session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
-			Queue queue = session.createQueue(QUEUE_NAME);
-			QueueReceiver receiver = session.createReceiver(queue);
-			receiver.setMessageListener(new MessageListener() {
-				@Override
-				public void onMessage(Message message) {
-					if (message != null) {
-						if (message instanceof TextMessage) {
-							TextMessage tm = (TextMessage) message;
-							try {
-								System.out.println("接收到消息:" + tm.getText());
-							} catch (JMSException e) {
-								e.printStackTrace();
-							}
+			while (true) {
+				QueueSession session = null;
+				try {
+					// true 使用session，AUTO_ACKNOWLEDGE 自动应答，自动删除消息
+					session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
+					Queue queue = session.createQueue(QUEUE_NAME);
+					QueueReceiver receiver = session.createReceiver(queue);
+					Message message = receiver.receive();
+					if (message instanceof TextMessage) {
+						TextMessage tm = (TextMessage) message;
+						try {
+							System.out.println("接收到消息:" + tm.getText() + ",Thread:" + Thread.currentThread().getId() + ",msgId:" + tm.getJMSMessageID());
+						} catch (JMSException e) {
+							e.printStackTrace();
 						}
 					}
+
+					// receiver.setMessageListener(new MessageListener() {
+					// @Override
+					// public void onMessage(Message message) {
+					// if (message != null) {
+					// if (message instanceof TextMessage) {
+					// TextMessage tm = (TextMessage) message;
+					// try {
+					// System.out.println("接收到消息:" + tm.getText());
+					// } catch (JMSException e) {
+					// e.printStackTrace();
+					// }
+					// }
+					// }
+					// }
+					// });
+
+					session.commit();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					close(session, null);
 				}
-			});
-
-			Thread.sleep(10 * 1000);
-			session.commit();
-
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			close(session, connection);
+			close(null, connection);
 		}
 	}
 
